23bd6e7a4a1c1c479ddc8252ddd653e3ddd157a5
Jordan Last <jordan.michael.last@gmail.com>
2023-02-23 09:31:07 -0600

    initial commit

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from kybra import query, update, void
+
+# This is a global variable that is stored on the heap
+message: str = ''
+
+# Query calls complete quickly because they do not go through consensus
+@query
+def get_message() -> str:
+    return message
+
+# Update calls take a few seconds to complete
+# This is because they persist state changes and go through consensus
+@update
+def set_message(new_message: str) -> void:
+    global message
+    message = new_message # This change will be persisted
